43cf2c1393ced309c517fc7ff90d4cf03fc7f088
Olga Piga <bondarolga@gmail.com>
2025-06-10 22:47:16 +0300

    initial commit

---
+++ src/test/java/com/example/MonitorSensors/MonitorSensorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MonitorSensors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorSensorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
